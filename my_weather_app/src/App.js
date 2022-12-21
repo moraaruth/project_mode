074b6613fd4860cd7ea68a36512eff7f0cee2433
Ruth Moraa <moraar37@gmail.com>
2022-12-21 10:59:58 +0300

    adding units when showing temperature

--- my_weather_app/src/App.js
+++ my_weather_app/src/App.js
@@ -1,16 +1,17 @@
 import hotBg from './assets/sunny.jpeg';
 import coldBg from './assets/cold.jpg';
 import Description from './components/Descriptions.jsx';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getWeatherData } from "./weather.js";
 
 function App() {
 
-  
+  const [weather, setWeather] = useState(null);
+  const [units, setUnits] = useState('imperial');
   useEffect(() => {
     const fetchWeatherData = async () => {
-      const data = await getWeatherData('nairobi');
-      console.log(data)
+      const data = await getWeatherData('nairobi', units);
+      setWeather(data)
 
     };
     fetchWeatherData();
@@ -21,27 +22,33 @@ function App() {
   return (
     <div className='app' style={{ background: `url(${coldBg})` }}>
       <div className='overlay'>
-        <div className='container'>
-          <div className='section section__inputs'>
-            <input type='text' name='city' placeholder='Enter City..'
-            />
-            <button>F</button>
+        {
+          weather && (
+            <div className='container'>
+              <div className='section section__inputs'>
+                <input type='text' name='city' placeholder='Enter City..'
+                />
+                <button>F</button>
 
-          </div>
-          <div className='section section__temperature'>
-            <div className='icon'>
-              <h3>Kenya, Nairobi</h3>
-              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
-              <h3>Cloudy</h3>
+              </div>
+              <div className='section section__temperature'>
+                <div className='icon'>
+                  <h3>{`${weather.name}, ${weather.country}`}</h3>
+                  <img src={weather.iconURL} alt="weatherIcon" />
+                  <h3>{weather.description}</h3>
+                </div>
+                <div className='temperature'>
+                  <h1>{`${weather.temp.toFixed()} ${units === 'metric' ? 'C' : 'F'}`}</h1>
+
+                </div>
+
+              </div>
+              <Description weather={weather} units={units}/>
             </div>
-            <div className='temperature'>
-              <h1>34 C</h1>
 
-            </div>
+          )
+        }
 
-          </div>
-          <Description />
-        </div>
       </div>
     </div>
 
